2d941e2e495e8a8faf4a06f677ef2f539ffb2782
genchamax <cool.mr.maksim@gmail.com>
2017-02-18 12:40:47 +0200

    Make project frame

---
+++ src/main/java/com/netcracker/OfficeManagementSystemApplication.java
@@ -0,0 +1,12 @@
+package com.netcracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfficeManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OfficeManagementSystemApplication.class, args);
+	}
+}
