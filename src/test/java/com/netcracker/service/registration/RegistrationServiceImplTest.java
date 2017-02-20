ae37765487d8a281e279ae055db9bb194212a4a5
Anatolii Syvenko <a.p.sivenko@gmail.com>
2017-02-20 11:41:24 +0200

    Added embedded DB for testing.

--- src/test/java/com/netcracker/service/registration/RegistrationServiceImplTest.java
+++ src/test/java/com/netcracker/service/registration/RegistrationServiceImplTest.java
@@ -10,6 +10,9 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Optional;
@@ -18,6 +21,7 @@ import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
 public class RegistrationServiceImplTest {
 
     @Autowired
