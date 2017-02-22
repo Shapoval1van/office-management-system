c773d7aefe1beb4dccffeb6f1215d40d3a8f993f
Anatolii Syvenko <a.p.sivenko@gmail.com>
2017-02-22 16:22:41 +0200

    Implemented Oauth2 with jwt.

---
+++ src/main/java/com/netcracker/config/JwtConfig.java
@@ -0,0 +1,43 @@
+package com.netcracker.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+import org.springframework.security.oauth2.provider.token.store.KeyStoreKeyFactory;
+
+@Configuration
+public class JwtConfig {
+
+    private static final String ALIAS = "auth";
+    private static final String PASSWORD = "syvenko";
+    private static final String KEY_RESOURCE = "jwt.jks";
+
+    @Bean
+    public TokenStore tokenStore() {
+        return new JwtTokenStore(accessTokenConverter());
+    }
+
+    @Bean
+    public JwtAccessTokenConverter accessTokenConverter() {
+        JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+        KeyStoreKeyFactory keyStoreKeyFactory =
+                new KeyStoreKeyFactory(new ClassPathResource(KEY_RESOURCE), PASSWORD.toCharArray());
+        converter.setKeyPair(keyStoreKeyFactory.getKeyPair(ALIAS));
+        return converter;
+    }
+
+    @Bean
+    @Primary
+    public DefaultTokenServices tokenServices() {
+        DefaultTokenServices defaultTokenServices = new DefaultTokenServices();
+        defaultTokenServices.setTokenStore(tokenStore());
+        defaultTokenServices.setSupportRefreshToken(true);
+        return defaultTokenServices;
+    }
+
+}
