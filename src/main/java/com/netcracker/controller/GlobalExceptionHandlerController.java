168d13cb0c9da15d18f00770e330a22a2a0c796b
Anatolii Syvenko <a.p.sivenko@gmail.com>
2017-02-24 19:59:12 +0200

    Added global exception handler.

---
+++ src/main/java/com/netcracker/controller/GlobalExceptionHandlerController.java
@@ -0,0 +1,14 @@
+package com.netcracker.controller;
+
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.RestController;
+
+@ControllerAdvice
+@RestController
+public class GlobalExceptionHandlerController {
+
+
+    }
+
+}
