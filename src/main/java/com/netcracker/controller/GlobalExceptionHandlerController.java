b79b54ccca0eb2f1a9cdac2249aec085ad78399a
Anatolii Syvenko <a.p.sivenko@gmail.com>
2017-02-25 14:16:05 +0200

    Changed error style.

--- src/main/java/com/netcracker/controller/GlobalExceptionHandlerController.java
+++ src/main/java/com/netcracker/controller/GlobalExceptionHandlerController.java
@@ -3,6 +3,7 @@ package com.netcracker.controller;
 
 import com.netcracker.exception.ResourceNotFoundException;
 import com.netcracker.model.dto.ErrorDTO;
+import com.netcracker.model.dto.ErrorsDTO;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -20,16 +22,16 @@ public class GlobalExceptionHandlerController {
 
     @ExceptionHandler(ResourceNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    public ErrorDTO resourceNotFoundExceptionHandler(HttpServletRequest request, ResourceNotFoundException ex){
-        ErrorDTO errorDTO = new ErrorDTO(HttpStatus.NOT_FOUND.value(),request.getRequestURL().toString(),ex.getMessage(),ex.getDescription());
-        return errorDTO;
+    public ErrorsDTO resourceNotFoundExceptionHandler(HttpServletRequest request, ResourceNotFoundException ex){
+        ErrorDTO error = new ErrorDTO(HttpStatus.NOT_FOUND.value(),request.getRequestURL().toString(),ex.getMessage(),ex.getDescription());
+        return new ErrorsDTO(Collections.singletonList(error));
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public List<ErrorDTO> handle(HttpServletRequest request, MethodArgumentNotValidException ex) {
+    public ErrorsDTO handle(HttpServletRequest request, MethodArgumentNotValidException ex) {
 
-        List<ErrorDTO> errorDTOS = ex.getBindingResult().getFieldErrors()
+        List<ErrorDTO> errors = ex.getBindingResult().getFieldErrors()
                 .stream()
                 .map(fieldError -> new ErrorDTO(HttpStatus.BAD_REQUEST.value(),
                         request.getRequestURL().toString(),
@@ -37,7 +39,7 @@ public class GlobalExceptionHandlerController {
                         fieldError.getField().concat(" - ").concat(fieldError.getDefaultMessage())))
                 .collect(Collectors.toList());
 
-        return errorDTOS;
+        return new ErrorsDTO(errors);
     }
 
 }
--- src/main/java/com/netcracker/model/dto/ErrorDTO.java
+++ src/main/java/com/netcracker/model/dto/ErrorDTO.java
@@ -1,6 +1,8 @@
 package com.netcracker.model.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ErrorDTO {
     public final static String TYPE = "errors";
     private int status;
