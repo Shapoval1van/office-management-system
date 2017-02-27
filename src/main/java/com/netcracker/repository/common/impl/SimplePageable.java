61402a4d3b0aa7d0b439a7e8b7160520d9b20e28
Anatolii Syvenko <a.p.sivenko@gmail.com>
2017-02-27 21:20:11 +0200

    Implemented Pageable.

---
+++ src/main/java/com/netcracker/repository/common/impl/SimplePageable.java
@@ -0,0 +1,36 @@
+package com.netcracker.repository.common.impl;
+
+
+import com.netcracker.repository.common.Pageable;
+
+public class SimplePageable implements Pageable {
+
+    private int pageSize;
+    private int pageNumber;
+
+    public SimplePageable() {
+    }
+
+    public SimplePageable(int pageSize, int pageNumber) {
+        this.pageSize = pageSize;
+        this.pageNumber = pageNumber;
+    }
+
+    public void setPageSize(int pageSize) {
+        this.pageSize = pageSize;
+    }
+
+    public void setPageNumber(int pageNumber) {
+        this.pageNumber = pageNumber;
+    }
+
+    @Override
+    public int getPageSize() {
+        return this.pageSize;
+    }
+
+    @Override
+    public int getPageNumber() {
+        return this.pageNumber;
+    }
+}
