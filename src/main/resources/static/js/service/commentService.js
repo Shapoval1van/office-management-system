eb31403f26ac3957cbf0c818f586e1cf557ad2c4
genchamax <cool.mr.maksim@gmail.com>
2017-02-28 13:21:23 +0200

    Add js service for WebSocket

---
+++ src/main/resources/static/js/service/commentService.js
@@ -0,0 +1,70 @@
+/**
+ * Created by Max on 27.02.2017.
+ */
+(function () {
+    angular.module("OfficeManagementSystem")
+        .service("CommentService", ["$q", "$timeout",
+            function ($q, $timeout) {
+                var service = {}, listener = $q.defer(), socket = {
+                    client: null,
+                    stomp: null
+                }, messageIds = [];
+
+                service.RECONNECT_TIMEOUT = 30000;
+                service.SOCKET_URL = "/chat";
+                service.CHAT_TOPIC = "/topic/request/";
+                service.CHAT_BROKER = "/app/chat";
+
+                service.receive = function () {
+                    return listener.promise;
+                };
+
+                service.send = function (message) {
+                    var id = Math.floor(Math.random() * 1000000);
+                    socket.stomp.send(service.CHAT_BROKER, {
+                        priority: 9
+                    }, JSON.stringify({
+                        message: message,
+                        id: id
+                    }));
+                    messageIds.push(id);
+                };
+
+                var reconnect = function () {
+                    $timeout(function () {
+                        initialize();
+                    }, this.RECONNECT_TIMEOUT);
+                };
+
+                var getMessage = function (data) {
+                    var message = JSON.parse(data), out = {};
+                    out.message = message.message;
+                    out.time = new Date(message.time);
+                    if (_.contains(messageIds, message.id)) {
+                        out.self = true;
+                        messageIds = _.remove(messageIds, message.id);
+                    }
+                    return out;
+                };
+
+                var startListener = function () {
+                    socket.stomp.subscribe(service.CHAT_TOPIC, function (data) {
+                        listener.notify(getMessage(data.body));
+                    });
+                };
+
+                var initialize = function () {
+                    socket.client = new SockJS(service.SOCKET_URL);
+                    socket.stomp = Stomp.over(socket.client);
+                    socket.stomp.connect({}, startListener);
+                    socket.stomp.onclose = reconnect;
+                };
+
+                service.initialize = function (requestId) {
+                    service.CHAT_TOPIC = service.CHAT_TOPIC + requestId;
+                    initialize();
+                };
+
+                return service;
+            }])
+})();
