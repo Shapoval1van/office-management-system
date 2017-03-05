2f83ea1d748c115e09ba6e6311d7ee6a44fd11a3
Anatolii Syvenko <a.p.sivenko@gmail.com>
2017-03-05 16:53:27 +0200

    Added auto reloading.

--- src/main/resources/static/js/service/sessionService.js
+++ src/main/resources/static/js/service/sessionService.js
@@ -6,6 +6,7 @@
 
         var currentUser = null;
         var currentSession = null;
+        var reloading = null;
 
         service.createSession = function (sessionInfo) {
             currentUser = {
@@ -20,7 +21,8 @@
                 accessTokenExpiresIn: new Date().getTime() + sessionInfo.data.expires_in * 1000,
                 accessToken : sessionInfo.data.access_token,
                 refreshToken : sessionInfo.data.refresh_token,
-                refreshTokenExpiresIn: new Date().getTime() + 1209600 * 1000
+                refreshTokenExpiresIn: new Date().getTime() + 1209600 * 1000,
+                reloadTime : sessionInfo.data.expires_in*1000 - 10*60*1000
             };
 
             localStorage.setItem("currentUser", JSON.stringify(currentUser));
@@ -28,6 +30,10 @@
             if (currentSession){
                 $http.defaults.headers.common.Authorization =
                     'Bearer ' + currentSession.accessToken;
+                if (reloading){
+                    clearInterval(reloading);
+                }
+                reloading = setInterval(service._reloadSession, currentSession.reloadTime);
             }
         };
 
@@ -36,44 +42,51 @@
             localStorage.removeItem("currentSession");
             currentUser = null;
             currentSession = null;
-        };
-
-        service.isOlderThanNow = function (time) {
-            return new Date().getTime() < time;
+            if (reloading){
+                clearInterval(reloading);
+            }
         };
 
         service.loadSession = function () {
             currentUser = JSON.parse(localStorage.getItem("currentUser"));
             currentSession = JSON.parse(localStorage.getItem("currentSession"));
             if (currentSession){
-                if (!service.isOlderThanNow(currentSession.accessTokenExpiresIn)) {
-                    if (service.isOlderThanNow(currentSession.refreshTokenExpiresIn)){
-                        var personCredentials = {
-                            grant_type: "refresh_token",
-                            refresh_token: currentSession.refreshToken,
-                            client_id: "client"
-                        };
-                        var encoded = btoa("client:");
-                        var req = {
-                            method: 'POST',
-                            url: "/oauth/token",
-                            headers: {
-                                "Authorization": "Basic " + encoded,
-                                "Content-type": "application/x-www-form-urlencoded; charset=utf-8"
-                            },
-                            data: $httpParamSerializer(personCredentials)
-                        };
-                        $http(req).then(function (callback) {
-                            service.createSession(callback);
-                        }, function (callback) {
-                            console.log("Error");
-                            console.log(callback);
-                        });
+                $http.defaults.headers.common.Authorization ='Bearer ' + currentSession.accessToken;
+                if (reloading){
+                    clearInterval(reloading);
+                }
+                reloading = setInterval(service._reloadSession, currentSession.reloadTime);
+                if (!service._isOlderThanNow(currentSession.accessTokenExpiresIn)) {
+                    if (service._isOlderThanNow(currentSession.refreshTokenExpiresIn)) {
+                        service._reloadSession();
                     }
                 }
-                    $http.defaults.headers.common.Authorization =
-                        'Bearer ' + currentSession.accessToken;
             }
+        };
+
+        service._reloadSession = function () {
+            console.log("Session was reloaded.");
+            var personCredentials = {
+                grant_type: "refresh_token",
+                refresh_token: currentSession.refreshToken,
+                client_id: "client"
+            };
+            var encoded = btoa("client:");
+            var req = {
+                method: 'POST',
+                url: "/oauth/token",
+                headers: {
+                    "Authorization": "Basic " + encoded,
+                    "Content-type": "application/x-www-form-urlencoded; charset=utf-8"
+                },
+                data: $httpParamSerializer(personCredentials)
+            };
+            $http(req).then(function (callback) {
+                service.createSession(callback);
+            }, function (callback) {
+                console.log("Error");
+                console.log(callback);
+            });
         };
 
         service.isUserLoggedIn = function () {
@@ -96,6 +109,9 @@
             return currentUser;
         };
 
+        service._isOlderThanNow = function (time) {
+            return new Date().getTime() < time;
+        };
 
         return service;
     }])
