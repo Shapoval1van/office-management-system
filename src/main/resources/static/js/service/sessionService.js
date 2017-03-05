70d8085a50046789ddd58ea78602ea8fe6e02742
Anatolii Syvenko <a.p.sivenko@gmail.com>
2017-03-05 15:53:13 +0200

    Added reloading access token after expiration.

--- src/main/resources/static/js/service/sessionService.js
+++ src/main/resources/static/js/service/sessionService.js
@@ -1,72 +1,102 @@
 (function () {
     angular.module("OfficeManagementSystem")
-        .service("SessionService", ["$http",
-            function ($http) {
-                var service = {};
+        .service("SessionService", ["$http", "$httpParamSerializer",
+    function ($http, $httpParamSerializer) {
+        var service = {};
 
-                var currentUser = null;
-                var currentSession = null;
+        var currentUser = null;
+        var currentSession = null;
 
-                service.createSession = function (sessionInfo) {
-                    currentUser = {
-                        firstName: sessionInfo.data.firstName,
-                        lastName: sessionInfo.data.lastName,
-                        id: sessionInfo.data.id,
-                        role: sessionInfo.data.type,
-                        email: sessionInfo.data.email
-                    };
+        service.createSession = function (sessionInfo) {
+            currentUser = {
+                firstName: sessionInfo.data.firstName,
+                lastName: sessionInfo.data.lastName,
+                id: sessionInfo.data.id,
+                role: sessionInfo.data.type,
+                email: sessionInfo.data.email
+            };
 
-                    currentSession = {
-                        accessToken : sessionInfo.data.access_token,
-                        expiresIn: new Date().getTime() + sessionInfo.data.expires_in * 1000,
-                        refreshToken : sessionInfo.data.refresh_token
-                    };
+            currentSession = {
+                accessTokenExpiresIn: new Date().getTime() + sessionInfo.data.expires_in * 1000,
+                accessToken : sessionInfo.data.access_token,
+                refreshToken : sessionInfo.data.refresh_token,
+                refreshTokenExpiresIn: new Date().getTime() + 1209600 * 1000
+            };
 
-                    localStorage.setItem("currentUser", JSON.stringify(currentUser));
-                    localStorage.setItem("currentSession", JSON.stringify(currentSession));
-                    if (currentSession){
-                        $http.defaults.headers.common.Authorization =
-                            'Bearer ' + currentSession.accessToken;
+            localStorage.setItem("currentUser", JSON.stringify(currentUser));
+            localStorage.setItem("currentSession", JSON.stringify(currentSession));
+            if (currentSession){
+                $http.defaults.headers.common.Authorization =
+                    'Bearer ' + currentSession.accessToken;
+            }
+        };
+
+        service.destroySession = function () {
+            localStorage.removeItem("currentUser");
+            localStorage.removeItem("currentSession");
+            currentUser = null;
+            currentSession = null;
+        };
+
+        service.isOlderThanNow = function (time) {
+            return new Date().getTime() < time;
+        };
+
+        service.loadSession = function () {
+            currentUser = JSON.parse(localStorage.getItem("currentUser"));
+            currentSession = JSON.parse(localStorage.getItem("currentSession"));
+            if (currentSession){
+                if (!service.isOlderThanNow(currentSession.accessTokenExpiresIn)) {
+                    if (service.isOlderThanNow(currentSession.refreshTokenExpiresIn)){
+                        var personCredentials = {
+                            grant_type: "refresh_token",
+                            refresh_token: currentSession.refreshToken,
+                            client_id: "client"
+                        };
+                        var encoded = btoa("client:");
+                        var req = {
+                            method: 'POST',
+                            url: "/oauth/token",
+                            headers: {
+                                "Authorization": "Basic " + encoded,
+                                "Content-type": "application/x-www-form-urlencoded; charset=utf-8"
+                            },
+                            data: $httpParamSerializer(personCredentials)
+                        };
+                        $http(req).then(function (callback) {
+                            service.createSession(callback);
+                        }, function (callback) {
+                            console.log("Error");
+                            console.log(callback);
+                        });
                     }
-                };
+                }
+                    $http.defaults.headers.common.Authorization =
+                        'Bearer ' + currentSession.accessToken;
+            }
+        };
 
-                service.destroySession = function () {
-                    localStorage.removeItem("currentUser");
-                    localStorage.removeItem("currentSession");
-                    currentUser = null;
-                    currentSession = null;
-                };
-
-                service.loadSession = function () {
-                    currentUser = JSON.parse(localStorage.getItem("currentUser"));
-                    currentSession = JSON.parse(localStorage.getItem("currentSession"));
-                    if (currentSession){
-                        $http.defaults.headers.common.Authorization =
-                            'Bearer ' + currentSession.accessToken;
-                    }
-                };
-
-                service.isUserLoggedIn = function () {
-                    if (currentSession && currentUser){
-                        var now = new Date().getTime();
-                        if (now < currentSession.expiresIn){
-                            return true;
-                        } else {
-                            return false;
-                        }
-                    }
+        service.isUserLoggedIn = function () {
+            if (currentSession && currentUser){
+                var now = new Date().getTime();
+                if (now < currentSession.accessTokenExpiresIn){
+                    return true;
+                } else {
                     return false;
-                };
+                }
+            }
+            return false;
+        };
 
-                service.getAccessToken = function () {
-                    return currentSession.accessToken;
-                };
+        service.getAccessToken = function () {
+            return currentSession.accessToken;
+        };
 
-                service.getCurrentUser = function () {
-                    return currentUser;
-                };
+        service.getCurrentUser = function () {
+            return currentUser;
+        };
 
 
-                return service;
-            }])
+        return service;
+    }])
 })();
