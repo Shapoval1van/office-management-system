10490bbe5915bfeea5f388b137de067a13c44976
Anatolii Syvenko <a.p.sivenko@gmail.com>
2017-03-13 17:54:18 +0200

    Extracted registration login to service.

--- src/main/resources/static/js/controller/registration/registrationController.js
+++ src/main/resources/static/js/controller/registration/registrationController.js
@@ -1,43 +1,25 @@
 (function () {
     angular.module("OfficeManagementSystem")
-        .controller("RegistrationController", ["$scope", "$http",
-            function ($scope, $http) {
-                $scope.personCredentials = {};
-                $scope.nameRegExp = /^[A-Z][a-zA-Z\d]{2,50}$/;
-                $scope.passRegExp = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/;
-                $scope.nameErrorMessage = "Name must contain at least 3 letters and start with capital letter!.";
-                $scope.emailErrorMessage="Invalid email address.";
-                $scope.passErrorMessage="The password must contain at least 8 characters, 1 capital letter and 1 number!";
-                $scope.confirmPassErrorMessage="Does not matches with the password!"
+        .controller("RegistrationController", ["$scope", "$http","RegistrationService",
+            function ($scope, $http, RegistrationService) {
 
-                $scope.responseEmail = "";
-                $scope.expiration = "";
+                $scope.personCredentials = {
+                    firstName:"",
+                    lastName:"",
+                    email:"",
+                    password:""
+                };
 
                 $scope.sendPersonCredentials = function () {
-                    $http.post("/api/v1/registration", $scope.personCredentials)
-                        .then(function (callback) {
-                            $scope.responseEmail = callback.data.email;
-                            $scope.expiration = callback.data.expiration;
-                            window.alert("To verify the data provided we have sent you a message to your email : "+$scope.responseEmail)
-                        }, function (callback) {
-                            window.alert("Registration Failure! This email is already used!")
+                    RegistrationService.registerEmployee($scope.personCredentials)
+                        .then(function (response) {
+                            if (response.isError){
+                                window.alert("Registration Failure!")
+                            } else {
+                                window.alert("To verify the data provided we have sent you a message to your email : "+response.responseEmail)
+                            }
 
                         })
                 };
             }])
-            .directive('pwCheck', [function () {
-                            return {
-                              require: 'ngModel',
-                              link: function (scope, elem, attrs, ctrl) {
-                                var firstPassword = '#' + attrs.pwCheck;
-                                elem.add(firstPassword).on('keyup', function () {
-                                  scope.$apply(function () {
-                                    var v = elem.val()===$(firstPassword).val();
-                                    ctrl.$setValidity('pwmatch', v);
-                                  });
-                                });
-                              }
-                            }
-                          }]);
-
 })();
--- src/main/resources/static/js/service/registration/registrationService.js
+++ src/main/resources/static/js/service/registration/registrationService.js
@@ -4,6 +4,16 @@
     function ($http, $httpParamSerializer) {
         var service = {};
 
+        service.registerEmployee = function (employee) {
+            var promise = $http.post("/api/v1/registration", employee)
+                .then(function (response) {
+                    response.isError = false;
+                }, function (response) {
+                    response.isError = true;
+                })
+            return promise;
+        };
+
         service.activateUser = function (token) {
             return $http.get("/api/v1/registration/" + token);
         };
