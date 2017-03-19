4e8629d60743f3dc4dabf32535fe48e2f15ec721
Anatolii Syvenko <a.p.sivenko@gmail.com>
2017-03-19 23:07:04 +0200

    Mixed update/show forms.

--- src/main/resources/static/js/controller/request/subController.js
+++ src/main/resources/static/js/controller/request/subController.js
@@ -3,7 +3,39 @@
         .controller("SubController", ["$scope","$routeParams", "SubService",
             function ($scope, $routeParams, SubService) {
                 var requestId = $routeParams.requestId;
-                $scope.subs = [];
+
+                $scope.subs = [
+                    {},
+                    {}
+                ];
+
+                angular.forEach($scope.employees, function (obj) {
+                    obj["showEdit"] = false;
+                });
+
+                $scope._toggleEdit = function (sub) {
+                    sub.showEdit = sub.showEdit ? false : true;
+                };
+
+                $scope.updateSub = function (sub) {
+
+                    $scope._toggleEdit(sub);
+                };
+
+                $scope.deleteSub = function (sub) {
+
+                };
+                
+                $scope.goEdit = function (sub) {
+
+                    $scope._toggleEdit(sub);
+                };
+
+                $scope.resetEdit = function (sub) {
+
+                    $scope._toggleEdit(sub);
+                };
+
                 $scope.formMode = "showing";
 
                 $scope.showNewSubtaskForm = function () {
