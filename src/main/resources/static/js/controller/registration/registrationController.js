8740858c921c4ae4eb9239f47efa8aad766f4401
Anatolii Syvenko <a.p.sivenko@gmail.com>
2017-03-19 14:31:22 +0200

    Added form validation to registration form.

--- src/main/resources/static/js/controller/registration/registrationController.js
+++ src/main/resources/static/js/controller/registration/registrationController.js
@@ -7,6 +7,9 @@
                     window.location = "/";
                 }
 
+                $scope.emailRegexp = new RegExp("^[\\w!#$%&’*+/=?`{|}~^-]+(?:\\.[\\w!#$%&’*+/=?`{|}~^-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$");
+                $scope.passwordRegexp = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[a-zA-Z\\d]{8,70}$");
+
                 $scope.person = {
                     firstName:"",
                     lastName:"",
@@ -14,6 +17,41 @@
                     password:"",
                     role:3,
                     enabled: false
+                };
+
+                $scope._errorPerson = {
+                    firstName:false,
+                    lastName:false,
+                    email:false,
+                    password:false
+                }
+
+                $scope._validate = function () {
+                    if ($scope.person.firstName==""||$scope.person.firstName.length<3){
+                        $scope._errorPerson.firstName = true;
+                    } else {
+                        $scope._errorPerson.firstName = false;
+                    }
+                    if ($scope.person.lastName==""||$scope.person.lastName.length<3){
+                        $scope._errorPerson.lastName = true;
+                    } else {
+                        $scope._errorPerson.lastName = false;
+                    }
+                    if (!$scope.emailRegexp.test($scope.person.email)){
+                        $scope._errorPerson.email = true;
+                    } else {
+                        $scope._errorPerson.email = false;
+                    }
+                    if (!$scope.passwordRegexp.test($scope.person.password)){
+                        $scope._errorPerson.password = true;
+                    } else {
+                        $scope._errorPerson.password = false;
+                    }
+
+                    return (!$scope._errorPerson.firstName&&
+                            !$scope._errorPerson.lastName&&
+                            !$scope._errorPerson.email&&
+                            !$scope._errorPerson.password);
                 };
 
                 $scope.roles = [
@@ -32,6 +70,9 @@
                 }
 
                 $scope.sendPersonCredentials = function () {
+                    if (!$scope._validate()){
+                        return;
+                    }
                     if ($scope.Session.getUserRole() == 'ROLE_ADMINISTRATOR'){
                         RegistrationService.registerAnyUser($scope.person)
                             .then(function (response) {
