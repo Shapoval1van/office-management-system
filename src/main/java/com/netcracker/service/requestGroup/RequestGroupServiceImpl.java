405c27c17e044ddef588feb97f1939e5f08043b7
genchamax <cool.mr.maksim@gmail.com>
2017-03-24 22:32:09 +0200

    Add getRequestGroupById method

--- src/main/java/com/netcracker/service/requestGroup/RequestGroupServiceImpl.java
+++ src/main/java/com/netcracker/service/requestGroup/RequestGroupServiceImpl.java
@@ -30,6 +30,8 @@ import static com.netcracker.util.MessageConstant.*;
 public class RequestGroupServiceImpl implements RequestGroupService {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(RequestGroupServiceImpl.class);
+    private static final Locale LOCALE = LocaleContextHolder.getLocale();
+
 
     @Autowired
     private MessageSource messageSource;
@@ -65,15 +67,13 @@ public class RequestGroupServiceImpl implements RequestGroupService {
     @Override
     @PreAuthorize("hasAnyAuthority('ROLE_OFFICE MANAGER', 'ROLE_ADMINISTRATOR')")
     public Optional<RequestGroup> saveRequestGroup(RequestGroup requestGroup) throws RequestGroupAlreadyExist {
-        Locale locale = LocaleContextHolder.getLocale();
-
         if (requestGroupRepository
                 .findRequestGroupByNameAndAuthor(requestGroup.getName(), requestGroup.getAuthor().getId()).isPresent()) {
             LOGGER.error(messageSource
-                    .getMessage(REQUEST_GROUP_ALREADY_EXIST, new Object[]{requestGroup.getName()}, locale));
+                    .getMessage(REQUEST_GROUP_ALREADY_EXIST, new Object[]{requestGroup.getName()}, LOCALE));
 
             throw new RequestGroupAlreadyExist(messageSource
-                    .getMessage(REQUEST_GROUP_ALREADY_EXIST, new Object[]{requestGroup.getName()}, locale));
+                    .getMessage(REQUEST_GROUP_ALREADY_EXIST, new Object[]{requestGroup.getName()}, LOCALE));
         }
 
         return requestGroupRepository.save(requestGroup);
@@ -82,21 +82,19 @@ public class RequestGroupServiceImpl implements RequestGroupService {
     @Override
     @PreAuthorize("hasAnyAuthority('ROLE_OFFICE MANAGER', 'ROLE_ADMINISTRATOR')")
     public Optional<RequestGroup> saveRequestGroup(RequestGroupDTO requestGroupDTO, Principal principal) throws CurrentUserNotPresentException, RequestGroupAlreadyExist {
-        Locale locale = LocaleContextHolder.getLocale();
-
         RequestGroup requestGroup = requestGroupDTO.toRequestGroup();
 
         String currentUserEmail = principal.getName();
         if (currentUserEmail.isEmpty()) {
             LOGGER.error("Current user not present");
             throw new CurrentUserNotPresentException(messageSource
-                    .getMessage(USER_ERROR_NOT_PRESENT, null, locale));
+                    .getMessage(USER_ERROR_NOT_PRESENT, null, LOCALE));
         }
         Optional<Person> currentUser = personRepository.findPersonByEmail(currentUserEmail);
         if (!currentUser.isPresent()) {
             LOGGER.error("Can't fetch information about current user");
             throw new CurrentUserNotPresentException(messageSource
-                    .getMessage(USER_WITH_EMAIL_NOT_PRESENT, new Object[]{currentUserEmail}, locale));
+                    .getMessage(USER_WITH_EMAIL_NOT_PRESENT, new Object[]{currentUserEmail}, LOCALE));
         } else
             requestGroup.setAuthor(currentUser.get());
 
@@ -106,24 +104,15 @@ public class RequestGroupServiceImpl implements RequestGroupService {
     @Override
     @PreAuthorize("hasAnyAuthority('ROLE_OFFICE MANAGER', 'ROLE_ADMINISTRATOR')")
     public Optional<RequestGroup> updateRequestGroup(RequestGroupDTO requestGroupDTO, Principal principal) throws ResourceNotFoundException, IllegalAccessException, RequestGroupAlreadyExist {
-        Locale locale = LocaleContextHolder.getLocale();
+        RequestGroup requestGroup = getRequestGroupById(requestGroupDTO.getId());
 
-        Optional<RequestGroup> requestGroupOptional = requestGroupRepository.findOne(requestGroupDTO.getId());
-
-        if (!requestGroupOptional.isPresent()) {
-            LOGGER.error("Request group with id {} not exist", requestGroupDTO.getId());
-            throw new ResourceNotFoundException(messageSource
-                    .getMessage(REQUEST_GROUP_NOT_EXIST, new Object[]{requestGroupDTO.getId()}, locale));
-        }
-
-        RequestGroup requestGroup = requestGroupOptional.get();
         if (requestGroupDTO.getName() != null) {
             requestGroup.setName(requestGroupDTO.getName());
         }
 
         if (!isAccessLegal(requestGroup, principal))
             throw new IllegalAccessException(messageSource
-                    .getMessage(REQUEST_GROUP_ILLEGAL_ACCESS, null, locale));
+                    .getMessage(REQUEST_GROUP_ILLEGAL_ACCESS, null, LOCALE));
 
         return saveRequestGroup(requestGroup);
     }
@@ -138,27 +127,19 @@ public class RequestGroupServiceImpl implements RequestGroupService {
     @PreAuthorize("hasAnyAuthority('ROLE_OFFICE MANAGER', 'ROLE_ADMINISTRATOR')")
     public void setRequestGroupStatus(Integer requestGroupId, Integer statusId, Principal principal)
             throws ResourceNotFoundException, IncorrectStatusException, IllegalAccessException {
-        Locale locale = LocaleContextHolder.getLocale();
 
-        Optional<RequestGroup> requestGroupOption = requestGroupRepository.findOne(requestGroupId);
+        RequestGroup requestGroup = getRequestGroupById(requestGroupId);
 
-        if (!requestGroupOption.isPresent()) {
-            LOGGER.error("Request group with id {} does not exist", requestGroupId);
-            throw new ResourceNotFoundException(messageSource
-                    .getMessage(REQUEST_ERROR_NOT_EXIST, new Object[]{requestGroupId}, locale));
-        }
-
-        if (!isAccessLegal(requestGroupOption.get(), principal))
+        if (!isAccessLegal(requestGroup, principal))
             throw new IllegalAccessException(messageSource
-                    .getMessage(REQUEST_GROUP_ILLEGAL_ACCESS, null, locale));
-
+                    .getMessage(REQUEST_GROUP_ILLEGAL_ACCESS, null, LOCALE));
 
         Optional<Status> statusOptional = statusRepository.findOne(statusId);
 
         if (!statusOptional.isPresent()) {
             LOGGER.error("Status with id {} not found", statusId);
             throw new ResourceNotFoundException(messageSource
-                    .getMessage(STATUS_ERROR, new Object[]{statusId}, locale));
+                    .getMessage(STATUS_ERROR, new Object[]{statusId}, LOCALE));
         }
 
         Status status = statusOptional.get();
@@ -167,8 +148,8 @@ public class RequestGroupServiceImpl implements RequestGroupService {
         if (statusName.equalsIgnoreCase(StatusEnum.FREE.getName()) ||
                 statusName.equalsIgnoreCase(StatusEnum.CANCELED.getName())) {
             LOGGER.error("Can't set status {} for group", statusName);
-            throw new IncorrectStatusException(messageSource.getMessage(STATUS_ERROR_NOT_AVAILABLE, null, locale),
-                    messageSource.getMessage(STATUS_ERROR_NOT_AVAILABLE_FOR_GROUP, new Object[]{statusName}, locale));
+            throw new IncorrectStatusException(messageSource.getMessage(STATUS_ERROR_NOT_AVAILABLE, null, LOCALE),
+                    messageSource.getMessage(STATUS_ERROR_NOT_AVAILABLE_FOR_GROUP, new Object[]{statusName}, LOCALE));
         }
 
         List<Request> requestsOfRequestGroup = requestRepository.findRequestsByRequestGroupId(requestGroupId);
@@ -189,6 +170,25 @@ public class RequestGroupServiceImpl implements RequestGroupService {
 
     }
 
+    /**
+     * Get request group
+     *
+     * @param requestGroupId
+     * @return request group
+     * @throws ResourceNotFoundException
+     */
+    public RequestGroup getRequestGroupById(Integer requestGroupId) throws ResourceNotFoundException {
+        Optional<RequestGroup> requestGroupOptional = requestGroupRepository.findOne(requestGroupId);
+
+        if (!requestGroupOptional.isPresent()) {
+            LOGGER.error("Request group with id {} does not exist", requestGroupOptional);
+            throw new ResourceNotFoundException(messageSource
+                    .getMessage(REQUEST_ERROR_NOT_EXIST, new Object[]{requestGroupId}, LOCALE));
+        }
+
+        return requestGroupOptional.get();
+    }
+
     private String generateRegexByNamePart(String namePart) {
         String regex = new StringBuilder()
                 .append(".*")
@@ -200,13 +200,12 @@ public class RequestGroupServiceImpl implements RequestGroupService {
     }
 
     private boolean isAccessLegal(RequestGroup requestGroup, Principal principal) throws CurrentUserNotPresentException, IllegalAccessException {
-        Locale locale = LocaleContextHolder.getLocale();
 
         Optional<Person> currentUser = personRepository.findPersonByEmail(principal.getName());
         if (!currentUser.isPresent()) {
             LOGGER.warn("Current user not present");
             throw new CurrentUserNotPresentException(messageSource
-                    .getMessage(USER_ERROR_NOT_PRESENT, null, locale));
+                    .getMessage(USER_ERROR_NOT_PRESENT, null, LOCALE));
         } else if (!currentUser.get().getId().equals(requestGroup.getAuthor().getId())) {
             Optional<Role> adminRole = roleRepository.findRoleByName(RoleEnum.ADMINISTRATOR.getName());
             if (!currentUser.get().getRole().getId().equals(adminRole.get().getId())) {
